fa46998d5bd1b3df93be1ac713735ac6782370b1
dev-akash-sarker <akashsarker210@gmail.com>
2023-08-11 15:20:57 +0600

    solve image

--- src/components/setting_component/index.js
+++ src/components/setting_component/index.js
@@ -69,12 +69,6 @@ const SettingLi = () => {
 
                   <h4>Edit Profile Name.</h4>
                 </div>
-                {/* <div className="image-editor">
-                  <Button onClick={handleOpenDialogTwo}>
-                    <BsImage />
-                  </Button>
-                  <h4>Edit Profile Photo.</h4>
-                </div> */}
               </div>
 
               <DiologeName
